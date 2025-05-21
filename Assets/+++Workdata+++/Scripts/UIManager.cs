9978b70e2938271bf4e3cae3c90b8f310595c8ef
NOTBOOKTOM\tomew <Tomwestendorf1@gmail.com>
2025-05-21 15:50:39 +0200

    erste Schritte (wortwörtlic) basic Movement und den stuff den wir schon vorher hatten

---
+++ Assets/+++Workdata+++/Scripts/UIManager.cs
@@ -0,0 +1,31 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private TMP_Text coinCountText;
+    [SerializeField] private GameObject panelLost;
+    [SerializeField] Button retryButton;
+
+    void Start()
+    {
+        panelLost.SetActive(false);
+        retryButton.onClick.AddListener(ReloadLevel);
+    }
+
+    void ReloadLevel()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ShowPanelLost()
+    {
+        panelLost.SetActive(true);
+    }
+    
+    public void UpdateCoinText(int newCoinCount)
+    {
+        coinCountText.text = newCoinCount.ToString();
+    }
+}
